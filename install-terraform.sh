e73ee98006fd8bf51eb2a4e013aa4f5f91e61e86
Cary Williams <cwilliams@iseatz.com>
2017-07-07 14:07:58 -0500

    updating terraform install

--- install-terraform.sh
+++ install-terraform.sh
@@ -1,54 +1,72 @@
 #!/usr/bin/env bash
+##################################################################################
+# Downloads and installs the specified version of terraform
+# Run using sudo unless your user has permissions to write to /usr/local/bin/
+##################################################################################
+
 set -e
-cd ~
 
-tver=0.9.5
-pver=1.0.0
-terraform=terraform_"${tver}"
-packer=packer_"${pver}"
+#===================================================================================
+# Usage function 
+#===================================================================================
+usage ()
+{
+        cat <<EOF
+
+	Usage : install-terraform.sh [new version]
+
+	Installs the version of terraform specified. Must be ran with sudo.
+        Specify the version numver you wish to install. A list of version can be found at:
+        https://www.terraform.io/downloads.html
+EOF
+    exit
+}
 
 
-# Create and move into terraform directory.
-
-if [ ! -d "~/${terraform}" ]; then
-    mkdir ~/$terraform
-else
-    exit 0
+#===================================================================================
+# Checks if using sudo
+#===================================================================================
+if [[ $(id -u) -ne 0 ]]; then
+    echo "Please run using sudo"
+    exit 3
 fi
 
-cd ~/"${terraform}"
-
-# Download Terraform. URI: https://www.terraform.io/downloads.html
-curl -O https://releases.hashicorp.com/terraform/$tver/"${terraform}"_linux_amd64.zip
-
-# Unzip and install terraform 
-unzip "${terraform}"_linux_amd64.zip
-
-cd ~
-if [ ! -d "~/${packer}" ]; then
-    mkdir ~/$packer
-else
-    exit 0
-fi
-cd ~/"${packer}"
-
-# Download Packer. URI: https://www.packer.io/downloads.html
-curl -O https://releases.hashicorp.com/packer/$pver/"${packer}"_linux_amd64.zip
-# Unzip and install
-unzip "${packer}"_linux_amd64.zip
-
-
-# Export terraform and packer paths
-
-if grep -q ${terraform} ~/.profile; then
-    echo "Your .profile already contains the path for terraform"
-    exit 0
-else
-    echo export PATH=~/${terraform}/:~/${packer}/:$PATH >>~/.profile
-    grep terraform ~/.profile
-    source ~/.profile
+#===================================================================================
+# Verifies a version is specified and assigns variables
+#===================================================================================
+if [ "$#" -ne 1 ]
+then
+    usage
 fi
 
-# verify we're all set to terraform and packer.
-terraform
-packer
+currentver="$(terraform -v |head -1 |awk '{print $2}')"
+newver=$1
+# Terraform.URL: https://www.terraform.io/downloads.html
+URL=https://releases.hashicorp.com/terraform/$newver/terraform_"${newver}"_linux_amd64.zip
+
+
+#===================================================================================
+# Downloads and installs terraform
+#===================================================================================
+
+if [ "${currentver}" == "v${newver}" ]; then
+   echo "You chose to install v${newver}, but you are already on that version."
+   exit 3
+else
+    echo "You are currently on version ${currentver}, and are installing ${newver}."
+    read -n2 -p "Do you wish to continue? (y/n) " choice
+    if [[ "${choice}" == 'y' ]] || [[ "${choice}" = 'Y' ]]
+    then
+	echo "Continuing with install of v${newver} ..."   
+        curl -O ${URL}
+        unzip terraform_"${newtver}"_linux_amd64.zip -d /usr/local/bin/
+        # Cleanup
+        rm terraform_"${newtver}"_linux_amd64.zip
+	# verify we're all set to terraform
+	terraform -v
+    fi
+fi
+
+
+   
+
