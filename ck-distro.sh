d53de7dec3e993591ecafc82a4f8b101cfb7d3fb
Cary Williams <cary@linux.com>
2019-10-05 14:57:33 -0500

    changing backtics

--- ck-distro.sh
+++ ck-distro.sh
@@ -1,52 +1,45 @@
 #!/bin/sh
 # Detects which OS and if it is Linux then it will detect which Linux distro is running.
 
-OS=`uname -s`
-REV=`uname -r`
-MACH=`uname -m`
+OS=$(uname -s)
+REV=$(uname -r)
+MACH=$(uname -m)
 
 GetVersionFromFile()
 {
-	VERSION=`cat $1 | tr "\n" ' ' | sed s/.*VERSION.*=\ // `
+	VERSION=$(cat $1 | tr "\n" ' ' | sed s/.*VERSION.*=\ // )
 }
 
 if [ "${OS}" = "SunOS" ] ; then
 	OS=Solaris
-	ARCH=`uname -p`	
-	OSSTR="${OS} ${REV}(${ARCH} `uname -v`)"
+	ARCH=$(uname -p)	
+	OSSTR="${OS} ${REV}(${ARCH} $(uname -v))"
 elif [ "${OS}" = "AIX" ] ; then
-	OSSTR="${OS} `oslevel` (`oslevel -r`)"
+	OSSTR="${OS} $(oslevel) ($(oslevel -r))"
 elif [ "${OS}" = "Linux" ] ; then
-	KERNEL=`uname -r`
+	KERNEL=$(uname -r)
 	if [ -f /etc/redhat-release ] ; then
 		DIST='RedHat'
-		PSUEDONAME=`cat /etc/redhat-release | sed s/.*\(// | sed 
-s/\)//`
-		REV=`cat /etc/redhat-release | sed s/.*release\ // | sed 
-s/\ .*//`
+		PSUEDONAME=$(cat /etc/redhat-release | sed s/.*\(// | sed s/\)//)
+		REV= `cat /etc/redhat-release | sed s/.*release\ // | sed s/\ .*//`
 	elif [ -f /etc/SuSE-release ] ; then
-		DIST=`cat /etc/SuSE-release | tr "\n" ' '| sed 
-s/VERSION.*//`
-		REV=`cat /etc/SuSE-release | tr "\n" ' ' | sed s/.*=\ 
-//`
+		DIST=$(cat /etc/SuSE-release | tr "\n" ' '| sed s/VERSION.*//)
+		REV=$(cat /etc/SuSE-release | tr "\n" ' ' | sed s/.*=\ //)
 	elif [ -f /etc/lsb-release ]; then
 	    DIST=""
-	    REV=`cat /etc/lsb-release |grep DISTRIB_DESC | grep -Eo '["\047].*["\047]'`
+	    REV=$(cat /etc/lsb-release |grep DISTRIB_DESC | grep -Eo '["\047].*["\047]')
 
 	elif [ -f /etc/mandrake-release ] ; then
 		DIST='Mandrake'
-		PSUEDONAME=`cat /etc/mandrake-release | sed s/.*\(// | 
-sed s/\)//`
-		REV=`cat /etc/mandrake-release | sed s/.*release\ // | 
-sed s/\ .*//`
+		PSUEDONAME=$(cat /etc/mandrake-release | sed s/.*\(// | sed s/\)//)
+		REV=$(cat /etc/mandrake-release | sed s/.*release\ // | sed s/\ .*//)
 	elif [ -f /etc/debian_version ] ; then
-		DIST="Debian `cat /etc/debian_version`"
+		DIST="Debian $(cat /etc/debian_version)"
 		REV=""
 
 	fi
 	if [ -f /etc/UnitedLinux-release ] ; then
-		DIST="${DIST}[`cat /etc/UnitedLinux-release | tr "\n" ' 
-' | sed s/VERSION.*//`]"
+		DIST="${DIST}[$(cat /etc/UnitedLinux-release | tr "\n" ' ' | sed s/VERSION.*//)]"
 	fi
 	
 	OSSTR="${DIST} ${REV}(${PSUEDONAME} ${KERNEL} ${MACH})"
